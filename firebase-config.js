92b6a012f6c9ff2e7a7467cb9dc6bc6456ff0413
motowashrp <robingaitan11@gmail.com>
2026-03-30 17:02:59 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey:            "AQUI_TU_VALOR",
-  authDomain:        "AQUI_TU_VALOR",
-  databaseURL:       "AQUI_TU_VALOR",
-  projectId:         "AQUI_TU_VALOR",
-  storageBucket:     "AQUI_TU_VALOR",
-  messagingSenderId: "AQUI_TU_VALOR",
-  appId:             "AQUI_TU_VALOR"
+  apiKey: "AIzaSyAp5NCH-vV4RIUFuD3FQ0CRit-44_1gBHo",
+  authDomain: "motowash-rp.firebaseapp.com",
+  databaseURL: "https://motowash-rp-default-rtdb.firebaseio.com",
+  projectId: "motowash-rp",
+  storageBucket: "motowash-rp.firebasestorage.app",
+  messagingSenderId: "1076586851362",
+  appId: "1:1076586851362:web:0205804af70f85c838ae7b"
 };
 
 export { firebaseConfig };
